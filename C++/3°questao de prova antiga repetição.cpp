a1122154b0c7abffb9198ad23bb5507c04e9e7e2
DANIEL LAWDRICK <161655702+DLawdrick@users.noreply.github.com>
2024-07-05 09:17:25 -0300

    Add files via upload

---
+++ C++/3°questao de prova antiga repetição.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <clocale>
+using namespace std;
+int main () {
+	int inicio, fim, soma;
+	cout<<"Informe o valor inicial:";
+	cin>>inicio;
+	cout<<"Informe o valor final:";
+	cin>>fim;
+	if (inicio % 2 == 1) {
+		inicio++;
+	
+} 
+soma = 0;
+for (int i = inicio; i <= fim; i = i + 2) {
+	soma = soma + i;
+	}
+	cout<<"A soma dos pares entre numeros informados e:"<<soma;
+	return 0;
+	}
