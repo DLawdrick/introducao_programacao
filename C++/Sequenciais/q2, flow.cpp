62ac4c52734c641ecc54a48509993903d5e6b4cf
DANIEL LAWDRICK <161655702+DLawdrick@users.noreply.github.com>
2024-07-09 11:52:04 -0300

    Add files via upload

---
+++ C++/Sequenciais/q2, flow.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <clocale>
+
+using namespace std;
+int main(){
+	setlocale(LC_ALL, "Portuguese");
+	
+int a;	
+cout<< "informe um numero: ";
+cin>> a;
+cout<< "o numero informado é: " << a;
+
+	
+	return 0;
+}
